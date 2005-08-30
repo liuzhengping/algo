57813954e1e413990420ca83eae5159002f5a6ca
Leo Osvald <leo.osvald@gmail.com>
2005-08-30 10:02:40 -0500

    Commit: Octohex.cpp

---
+++ acsl/intermediate/all-star/Octohex.cpp
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <iostream.h>
+#include <iomanip.h>
+int main() {
+    long long int a, b;
+    scanf("%llo %llo", &a, &b);
+    printf("%llx", a*b);
+    getch();
+    return 0;
+}    
